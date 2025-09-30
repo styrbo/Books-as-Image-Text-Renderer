a4f4cc04e6d7415f8d88a4445dadb396612e3e02
styrbo <styrboCanel@gmail.com>
2025-09-30 22:44:02 +0400

    init commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,110 @@
+﻿using ConsoleApp1;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+var fontPath = $@"{Environment.CurrentDirectory}\Resources\Minecraftia-Regular v2.ttf";
+var textPath = $@"{Environment.CurrentDirectory}\Resources\test.txt";
+var canvasWidth = 2048;
+var canvasMaxHeight = 10000;
+var maxLettersPerImage = 10000;
+var downscaleFactor = 4;
+float fontSize = 32f;
+
+Console.WriteLine("start");
+
+var backgroundColor = Rgba32.ParseHex("#00000000");
+var textColor = Rgba32.ParseHex("#000000");
+
+var drawingOptions = new DrawingOptions {
+    GraphicsOptions = new GraphicsOptions {
+        Antialias = false,
+    },
+};
+var brush = new SolidBrush(textColor);
+
+var text = System.IO.File.ReadAllText(textPath);
+var textSegments = RichTextParser.Parse(text, fontSize, TextAlignment.Start);
+var wordsBySegmentId = textSegments.SelectMany(segment => 
+    segment.Text.Split(" ")
+        .Select(word => ($" {word}", Array.IndexOf(textSegments, segment))))
+    .ToArray();
+
+var fontsCollection = textSegments
+    .GroupBy(segment => segment.size)
+    .ToDictionary(group => group.Key, group => {;
+        var collection = new FontCollection().Add(fontPath);
+        return collection.CreateFont(group.Key, FontStyle.Regular);
+    });
+
+var textTotalHeight = 0f;
+var optionsPerSegment = textSegments.Select(segment => {
+    var option = new RichTextOptions(fontsCollection[segment.size]) {
+        LineSpacing = 1.5f,
+        Origin = new PointF(new SizeF(height: textTotalHeight,
+            width: segment.textAlignment == TextAlignment.Center ? canvasWidth / 2f : 0)), // это baseline
+        VerticalAlignment = VerticalAlignment.Top,
+        HorizontalAlignment = (HorizontalAlignment)segment.textAlignment,
+        WordBreaking = WordBreaking.BreakWord,
+        WrappingLength = canvasWidth,
+    };
+    
+    var textSize = TextMeasurer.MeasureSize(segment.Text, option);
+    textTotalHeight += (int)textSize.Height;
+
+    return option;
+}).ToArray();
+
+var builder = new System.Text.StringBuilder();
+var breakPoints = new List<int>();
+for (var i = 0; i < wordsBySegmentId.Length; i++) {
+    var (word, segmentId) = wordsBySegmentId[i];
+    builder.Append(word);
+    var linesCount = TextMeasurer.CountLines(builder.ToString(), optionsPerSegment[segmentId]);
+    if (linesCount > 1) {
+        breakPoints.Add(i);
+        builder.Clear();
+    }
+}
+
+
+var imagesNeededToFitAllText = (int)Math.Ceiling(textTotalHeight / canvasMaxHeight);
+var processedTextHeight = 0;
+/*
+for (int i = 0; i < imagesNeededToFitAllText; i++) {
+    var heightLeft = Math.Min(canvasMaxHeight, (int) Math.Ceiling(textTotalHeight - processedTextHeight));
+    using var image = new Image<Rgba32>(canvasWidth, heightLeft, backgroundColor);
+    image.Mutate(ctx => ctx
+        .DrawText(drawingOptions, richTextOptions, richTextSegmentData.Text, brush, null)
+    );
+    
+    var textSize = TextMeasurer.MeasureSize(richTextSegmentData.Text, richTextOptions);
+    offset.Height += (int)textSize.Height;
+}
+
+var offset = Size.Empty;
+foreach (var richTextSegmentData in textSegments) {
+    var collection = new FontCollection().Add(fontPath);
+    var font = collection.CreateFont(richTextSegmentData.size, FontStyle.Regular);
+
+    RichTextOptions richTextOptions = new(font) {
+        LineSpacing = 1.5f,
+        Origin = new PointF(new SizeF(height: offset.Height, width: richTextSegmentData.textAlignment == TextAlignment.Center ? canvasSize.x / 2f : 0)), // это baseline
+        VerticalAlignment = VerticalAlignment.Top,
+        HorizontalAlignment = (HorizontalAlignment) richTextSegmentData.textAlignment,
+        WordBreaking = WordBreaking.BreakWord,
+        WrappingLength = canvasSize.x,
+    };
+}
+
+image.Mutate(ctx => 
+    ctx.Resize(new Size(canvasSize.x / downscaleFactor, canvasSize.y / downscaleFactor), KnownResamplers.NearestNeighbor, false)
+    );
+//downscale at halp size
+image.SaveAsPng(@$"{Environment.CurrentDirectory}\output.png");
+
+Console.WriteLine("finish");
+Console.ReadKey();
+*/
---
+++ ConsoleApp1/RichTextParser.cs
@@ -0,0 +1,94 @@
+﻿using SixLabors.Fonts;
+
+namespace ConsoleApp1;
+
+public static class RichTextParser {
+
+    public static RichTextSegmentData[] Parse(string text, float defaultSize, TextAlignment defaultAlignment) {
+        var firstTagIndex = text.IndexOf('<');
+        if(firstTagIndex == -1) {
+            return [new RichTextSegmentData(text, defaultSize, defaultAlignment)];
+        }
+        
+        var segments = new List<RichTextSegmentData>();
+
+        var i = 0;
+        var lastTagIndex = 0;
+        while (i < text.Length) {
+            var currentChar = text[i];
+            if (currentChar == '<') {
+                var closingTagIndex = text.IndexOf('>', i);
+                if (closingTagIndex == -1) {
+                    // No closing tag found, treat the rest as normal text
+                    i++;
+                } else {
+                    var tagContent = text.Substring(i + 1, closingTagIndex - i - 1);
+                    var tagParts = tagContent.Split('=');
+                    var tagName = tagParts[0].ToLower();
+                    var tagValue = tagParts.Length > 1 ? tagParts[1] : null;
+                    var endSegmentIndex = text.IndexOf($"</{tagName}", closingTagIndex + 1, StringComparison.Ordinal);
+                    if (endSegmentIndex == -1) {
+                        endSegmentIndex = text.Length;
+                    }
+                    var segmentText = text.Substring(closingTagIndex + 1, endSegmentIndex - closingTagIndex - 1);
+                    var innerData = new RichTextSegmentData(segmentText, defaultSize, defaultAlignment);
+                    var taggedData = TagCollection(innerData, tagName, tagValue);
+                    
+                    if(lastTagIndex > 0)
+                        segments.Add(new RichTextSegmentData(text.Substring(lastTagIndex, i - lastTagIndex), defaultSize, defaultAlignment));
+                    
+                    segments.Add(taggedData);
+                    
+                    i = endSegmentIndex + tagName.Length + 3;
+                    lastTagIndex = i;
+                }
+            } else {
+                i++;
+            }
+        }
+        
+        if(lastTagIndex < text.Length) {
+            segments.Add(new RichTextSegmentData(text.Substring(lastTagIndex), defaultSize, defaultAlignment));
+        }
+
+        return segments.ToArray();
+}
+
+
+    private static RichTextSegmentData TagCollection(RichTextSegmentData innerData, string tagName, string? tagValue) {
+        switch (tagName) {
+            case "size":
+                if (float.TryParse(tagValue, out var newSize)) {
+                    return innerData with { size = newSize };
+                }
+                return innerData;
+            case "align":
+
+                TextAlignment? newAlignment = null;
+                switch (tagValue) {
+                    case "left":
+                        newAlignment = TextAlignment.Start;
+                        break;
+                    case "center":
+                        newAlignment = TextAlignment.Center;
+                        break;
+                    case "right":
+                        newAlignment = TextAlignment.End;
+                        break;
+                    default:
+                        newAlignment = null;
+                        break; 
+                }
+                
+                if (newAlignment != null) {
+                    return innerData with { textAlignment = newAlignment.Value };
+                }
+                return innerData;
+            default:
+                return innerData;
+        }
+        
+    }
+}
+
+public readonly record struct RichTextSegmentData(string Text, float size, TextAlignment textAlignment);

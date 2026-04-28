eb686b217cb1daf01318e1b0b6cc252d4f496402
styrbo <styrboCanel@gmail.com>
2026-04-29 03:25:45 +0400

    last comit i guess :p

---
+++ ConsoleApp1/ConsoleDrawer.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1;
+
+public static class ConsoleDrawer {
+    public static int DrawError(string errorMessage) {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"error: {errorMessage}");
+        Console.ReadKey();
+        return -1;
+    }
+
+    public static void DrawText(string text) {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(text);
+        Console.ResetColor();
+    }
+    
+    public static bool AskPermissionToContinue(string message) {
+        while (true) {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{message} [Y/N]");
+            Console.ResetColor();
+            var key = Console.ReadKey();
+
+            switch (key.Key) {
+                case ConsoleKey.Y:
+                    return true;
+                case ConsoleKey.N:
+                    return false;
+            }
+        }
+    }
+}
